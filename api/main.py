44ad2ad4e51dddcab869eaa2c0a782960fbcffd6
Alex McColm <mccolmalex@gmail.com>
2025-01-05 21:25:10 -0800

    Add CORS middleware, allow localhost: npm ports to access

--- api/main.py
+++ api/main.py
@@ -1,8 +1,23 @@
 import random
 import time
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+    "localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Load word lists
 eightWords = open('eight.txt', 'r').read().split('\n')
